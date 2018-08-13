52ff4fcee43c8c5c3a3cd1ff597bbaced75d98b2
kraftme90 <35199591+kraftme90@users.noreply.github.com>
2018-08-13 10:29:05 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 //init the required packages
 const express = require('express');
-const exphbs = require('express-handlebars');
-const app = express();
 const mysql = require('./dbcon.js');
+const bodyParser = require('body-parser');
+
+const app = express();
+const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 // init the view engine
-app.engine('hbs', exphbs({defaultLayout: 'main.hbs'}));
+app.engine('hbs', exphbs.create({defaultLayout:'main'}));
 app.set('view engine', 'hbs');
 
 // serving the static pages
-app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 app.set('mysql', mysql);
 
 app.use('/prescription', require('./prescription.js'));
@@ -18,7 +23,6 @@ app.use('/otc', require('./otc.js'));
 //serving the main page
 app.get('/', function(req, res, next)  {
    res.status(200);
-   // res.render('helloWorld.handlebars');
 
 // get username
    var userName = 'Stan';
@@ -87,15 +91,15 @@ app.use(function(err, req, res, next){
     res.render('500');
 });
 
-//setting the port to the env variable or 3000 as default
+/*setting the port to the env variable or 3000 as default
  var port = process.env.PORT || 3000;
  app.listen(port);
-
+*/
 
 /**************************************************************
  *  alternate listening port
  *   *************************************************************/
-//app.set('port', 4856);
-//app.listen(app.get('port'), function(){
-//    console.log('Express started http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-//});
+app.set('port', 4857);
+app.listen(app.get('port'), function(){
+    console.log('Express started http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
