10c4dbd23566f0c9220f601224c207639d0ca0d9
kraftme90 <35199591+kraftme90@users.noreply.github.com>
2018-08-14 21:24:47 -0400

    Add files via upload

--- otc.js
+++ otc.js
@@ -18,8 +18,8 @@ module.exports = function(){
 
   router.get('/insertmed',function(req,res,next){																			// Insert new row into schools table
     var context = {};
-    mysql.pool.query("INSERT INTO `medication` (`name`, `date_entered`, `start_date`, `expir_date`, `quantity`, `quantity_unit`, `dosage`, `dosage_unit`, `frequency`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 			// Submit query to insert new data into schools table
-      [req.query.name, req.query.date_entered, req.query.start_date, req.query.expir_date, req.query.quantity, req.query.quantity_unit, req.query.dosage, req.query.dosage_unit, req.query.frequency], 
+    mysql.pool.query("INSERT INTO `medication` (`name`, `start_date`, `expir_date`, `quantity`, `quantity_unit`, `dosage`, `dosage_unit`, `instructions`, `duration`, `frequency`, `date_entered`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 			// Submit query to insert new data into schools table
+      [req.query.name, req.query.start_date, req.query.expir_date, req.query.quantity, req.query.quantity_unit, req.query.dosage, req.query.dosage_unit, req.query.instructions, req.query.durations, req.query.frequency, req.query.date_entered], 
       function(err, result){
         if(err){
           next(err);
